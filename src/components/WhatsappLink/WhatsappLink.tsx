9e371b51628cbb095e41c684c7c07300dc40118c
Mouhcine Fallahi <mohcinefallahi23@gmail.com>
2025-08-25 16:44:09 +0100

    add: finish app

--- src/components/WhatsappLink/WhatsappLink.tsx
+++ src/components/WhatsappLink/WhatsappLink.tsx
@@ -5,7 +5,7 @@ const WhatsappLink = () => {
   return (
     <a
       className="whatsapp-link"
-      href="https://wa.me/212666926248?text=Bonjour%20Docteur%2C%20je%20voudrais%20r%C3%A9server%20un%20rendez-vous%20le%20%5Bdate%5D%20%C3%A0%20%5Bheure%5D"
+      href="https://wa.me/212666666666?text=Bonjour%20Docteur%2C%20je%20voudrais%20r%C3%A9server%20un%20rendez-vous%20le%20%5Bdate%5D%20%C3%A0%20%5Bheure%5D"
       target="_blank"
       rel="noopener noreferrer"
     >
